8f5406c209c01a0bb374eac87d4166566fcb559c
MoMo <momoforever@vip.qq.com>
2017-06-21 23:04:47 +0800

    add service_add and service_modify function and html

--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^add/$', views.service_add, name='service_add'),
-    url(r'^detail/$', views.service_detail, name='service_detail'),
+    url(r'^detail/(?P<service_id>\w+)', views.service_detail, name='service_detail'),
+    url(r'^modify/(?P<service_id>\w+)', views.service_modify, name='service_modify'),
 ]
--- services/views.py
+++ services/views.py
@@ -35,9 +35,39 @@ def service_add(request):
     }
     return render(request, template, context)
 
-def service_detail(request):
+def service_detail(request, service_id):
+    single_service = Service.objects.get(service_id=service_id)
+    print('single_service: % s' % single_service)
     template = "service_detail.html"
     context = {
+        "single_service":single_service
+    }
+    return render(request, template, context)
 
+def service_modify(request, service_id):
+    single_service = Service.objects.get(service_id=service_id)
+    if request.method == 'POST':
+        #check the post value
+        print(request.POST)
+        form = ServiceInfoForm(request.POST)
+        if form.is_valid():
+            print('form is valid!')
+            servicename = form.cleaned_data['service_name']
+            maxnodes = form.cleaned_data['max_nodes']
+            url = form.cleaned_data['url']
+            description = form.cleaned_data['description']
+            single_service.service_name = servicename
+            single_service.max_nodes = maxnodes
+            single_service.url = url
+            single_service.description = description
+            single_service.save()
+            return HttpResponseRedirect('/services/')
+        else:
+            print("form is not valid!!!")
+    else:
+        form = ServiceInfoForm()
+    template = "service_modify.html"
+    context = {
+        "single_service": single_service
     }
     return render(request, template, context)
