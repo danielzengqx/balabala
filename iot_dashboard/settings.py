e9b8ffd45b9876226c026dd4645a24ad75348a0e
danielzengqx <danielzengqx@users.noreply.github.com>
2019-04-21 13:20:48 +0800

    Update settings.py

--- iot_dashboard/settings.py
+++ iot_dashboard/settings.py
@@ -155,6 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER='cheer_zeng@163.com'
-EMAIL_HOST_PASSWORD = 'mail789'
 
 DEFAULT_FROM_EMAIL = 'cheer_zeng@163.com'
